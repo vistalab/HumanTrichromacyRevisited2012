5bc3b46527d43d7d1db7083cadd10170dd672e67
Jonathan Winawer <jonathan.winawer@nyu.edu>
2014-01-17 10:01:50 -0500

    changed path from biac to local root path

--- utilities/cm_defaultPathforSavefigure.m
+++ utilities/cm_defaultPathforSavefigure.m
@@ -1,12 +1,12 @@
 function path = cm_defaultPathforSavefigure
+% Set path to save figures.
 %
-%
+% path = cm_defaultPathforSavefigure
 %
 %%
-% path = fullfile('~','PNASfigures');
 
-path = fullfile('/','biac4','wandell','biac3','wandell7','hhiro','PNASfigures');
+path = fullfile(cmPublicRootPath,'PNASfigures');
 
-if ~exist(path,'dir')
-   mkdir(path) 
-end
+if ~exist(path,'dir'), mkdir(path); end
+
+return
